560004db961e97f671473fac90dcaf4ce1979897
Simmigon <simmigon.flagg@gmail.com>
2018-08-15 19:14:59 -0400

    Adding database config

--- js/app.js
+++ js/app.js
@@ -1,64 +1,49 @@
-    // ========================================== START CODING BELOW!!
+var name = "";
+var email = "";
+var age = 0;
+var comment = "";
 
-    // Initialize Firebase
-    var config = {
-      apiKey: "AIzaSyCanlYIc7n-Wel8wDeaMxMzYtViVVCOwpI",
-      authDomain: "recent-user-with-push.firebaseapp.com",
-      databaseURL: "https://recent-user-with-push.firebaseio.com",
-      storageBucket: "recent-user-with-push.appspot.com",
-      messagingSenderId: "208476116054"
-    };
+// Capture Button Click
+$("#add-user").on("click", function (event) {
+	event.preventDefault();
 
-    firebase.initializeApp(config);
+	// Grabbed values from text boxes
+	name = $("#name-input").val().trim();
+	email = $("#role-input").val().trim();
+	age = $("#date-input").val().trim();
+	comment = $("#rate-input").val().trim();
 
-    // Create a variable to reference the database.
-    var database = firebase.database();
+	console.log(name);
+	// Code for handling the push
+	database.ref().push({
+		name: name,
+		email: email,
+		age: age,
+		comment: comment,
+		dateAdded: firebase.database.ServerValue.TIMESTAMP
+	});
 
-    // Initial Values
-    var name = "";
-    var email = "";
-    var age = 0;
-    var comment = "";
+});
 
-    // Capture Button Click
-    $("#add-user").on("click", function(event) {
-      event.preventDefault();
+// Firebase watcher .on("child_added"
+database.ref().on("child_added", function (snapshot) {
+	// storing the snapshot.val() in a variable for convenience
+	var sv = snapshot.val();
 
-      // Grabbed values from text boxes
-      name = $("#name-input").val().trim();
-      email = $("#email-input").val().trim();
-      age = $("#age-input").val().trim();
-      comment = $("#comment-input").val().trim();
+	// Console.loging the last user's data
+	console.log(sv.name);
+	console.log(sv.email);
+	console.log(sv.age);
+	console.log(sv.comment);
 
-      // Code for handling the push
-      database.ref().push({
-        name: name,
-        email: email,
-        age: age,
-        comment: comment,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
+	// Change the HTML to reflect
+	$("#name-display").text(sv.name);
+	$("#role-input").text(sv.email);
+	$("#date-inputy").text(sv.age);
+	$("#rate-input").text(sv.comment);
+	
 
-    });
-
-    // Firebase watcher .on("child_added"
-    database.ref().on("child_added", function(snapshot) {
-      // storing the snapshot.val() in a variable for convenience
-      var sv = snapshot.val();
-
-      // Console.loging the last user's data
-      console.log(sv.name);
-      console.log(sv.email);
-      console.log(sv.age);
-      console.log(sv.comment);
-
-      // Change the HTML to reflect
-      $("#name-display").text(sv.name);
-      $("#email-display").text(sv.email);
-      $("#age-display").text(sv.age);
-      $("#comment-display").text(sv.comment);
-
-      // Handle the errors
-    }, function(errorObject) {
-      console.log("Errors handled: " + errorObject.code);
-    });
+	// Handle the errors
+}, function (errorObject) {
+	console.log("Errors handled: " + errorObject.code);
+});
